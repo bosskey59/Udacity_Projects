c77d75d05b36ccd172adfe4f5b982d330ac69be3
Alejandro Aguilar <AlejandroAguilar@Alejandros-Air.attlocal.net>
2016-09-01 11:25:55 -0500

    feat: added yelpapi

--- NeighborhoodMap_AlejandroAguilarJr/js/script.js
+++ NeighborhoodMap_AlejandroAguilarJr/js/script.js
@@ -18,19 +18,23 @@ var infowindow = new google.maps.InfoWindow();
               });
             
 
-              google.maps.event.addListener(marker, 'click', function() {
-                  infowindow.setContent(location.title);
-                  infowindow.open(map, this);
-                  map.panTo(location.position); 
-                  marker.setAnimation(google.maps.Animation.BOUNCE);
-                  setTimeout(function(){marker.setAnimation(null);}, 1450);
+            google.maps.event.addListener(marker, 'click', function() {
+                content='<h3><a href="'+location.url+'">'+location.title+'</h3>'+'<img src="'+location.snippetImgURL +'"">'
+                infowindow.setContent(content);
+                infowindow.open(map, this);
+                map.panTo(location.position); 
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+                setTimeout(function(){marker.setAnimation(null);}, 1450);
 
               });
               return marker;
     };
 
+    
+
     for(var i=0;i<markers.length;i++){
-    markers[i].marker=setMarker(markers[i]);
+        getData(i); 
+        markers[i].marker=setMarker(markers[i]);
 };
 
 ko.applyBindings(new viewModel());
@@ -40,38 +44,43 @@ ko.applyBindings(new viewModel());
 var markers = [
 
 {
-    position:{lat: 27.5299328, lng: -99.4702068},
+    position:{lat: 27.5301843, lng: -99.4701397},
     map:map,
-    title: 'Safari',
-    marker:''
+    title: 'Ravi',
+    marker:'',
+    yelp:'https://api.yelp.com/v2/business/taquitos-ravi-restaurant-laredo-3'
 },
 
 {
     position:{lat: 27.53582, lng: -99.4847241},
     map:map,
     title: 'Dannys',
-    marker:''
+    marker:'',
+    yelp:'https://api.yelp.com/v2/business/dannys-laredo'
 },
 
 {
     position:{lat: 27.566054, lng: -99.473789},
     map:map,
     title: 'Bolillos',
-    marker:''
+    marker:'',
+    yelp:'https://api.yelp.com/v2/business/bolillos-cafe-laredo'
 },
 
 {
     position:{lat: 27.5085654, lng: -99.4760447},
     map:map,
     title: 'Rauls BBQ',
-    marker:''
+    marker:'',
+    yelp:'https://api.yelp.com/v2/business/rauls-bbq-laredo'
 },
 
 {
     position:{lat: 27.506831, lng: -99.476526},
     map:map,
     title: 'TPs',
-    marker:''
+    marker:'',
+    yelp:'https://api.yelp.com/v2/business/taco-palenque-laredo-7?osq=Taco+Palenque'
 }
 
 ];
@@ -106,6 +115,74 @@ var viewModel = function() {
 
 
 };
+
+var getData= function(x){
+
+    var yelp=markers[x].yelp;
+    var auth = { 
+      //
+      // Update with your auth tokens.
+      //
+      consumerKey: "NvthsWb2e1em8ACUnPZJ7Q", 
+      consumerSecret: "Xk94g9QCsokikQGa15b3GNCL-KA",
+      accessToken: "voTVWiw8iRXDV6wE7HW8gfwfQdBuEH9w",
+      // This example is a proof of concept, for how to use the Yelp v2 API with javascript.
+      // You wouldn't actually want to expose your access token secret like this in a real application.
+      accessTokenSecret: "Giq18Hp87NY9cyMw4JBF5Hrq87k",
+      serviceProvider: { 
+        signatureMethod: "HMAC-SHA1"
+      }
+    };
+
+    var ll = '27.530560, -99.480359';
+    var radius = '4000';
+
+    var accessor = {
+      consumerSecret: auth.consumerSecret,
+      tokenSecret: auth.accessTokenSecret
+    };
+
+    parameters = [];
+    parameters.push(['ll', ll]);
+    parameters.push(['radius', radius]);
+    parameters.push(['callback', 'cb']);
+    parameters.push(['oauth_consumer_key', auth.consumerKey]);
+    parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+    parameters.push(['oauth_token', auth.accessToken]);
+    parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+
+    var message = { 
+      'action': yelp,
+      'method': 'GET',
+      'parameters': parameters 
+    };
+
+    OAuth.setTimestampAndNonce(message);
+    OAuth.SignatureMethod.sign(message, accessor);
+
+    var parameterMap = OAuth.getParameterMap(message.parameters);
+    parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature)
+    console.log(parameterMap);
+
+
+
+    $.ajax({
+      'url': yelp,
+      'data': parameterMap,
+      'cache': true,
+      'dataType': 'jsonp',
+      'callback': 'cb',
+      'success': function(data) {
+        console.log(data);
+        markers[x].url= data.url;
+        markers[x].snippetImgURL= data.image_url;
+
+
+      }
+    });
+};
+
+
 
 
 
